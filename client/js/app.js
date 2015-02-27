bcf4df44c956e6a8b59afc65c08a54ee0c90f08d
joster422 <joster422@gmail.com>
2015-02-27 03:39:25 -0500

    Filtering done finally

--- client/js/app.js
+++ client/js/app.js
@@ -22,45 +22,58 @@ var sites = [{
 	name: 'CLT W',
 	address: '5101 Westinghouse Boulevard',
 	address2: 'Charlotte, NC 28273',
-	selected: false
+	selected: true
 }, {
 	name: 'HOU T',
 	address: '10730 Telge Road',
 	address2: 'Houston, TX 77095',
-	selected: false
+	selected: true
 }, {
 	name: 'JUP C',
 	address: '1680 South Central Boulevard',
 	address2: 'Jupiter, FL 33458',
-	selected: false
+	selected: true
 }];
 
-var tags = [{
+var groups = [{
 	name: 'Siemens Corporate',
+	image: 'assets/Siemens.png',
 	selected: true
 }, {
 	name: 'Disability Support Network',
+	image: 'assets/bridge.png',
 	selected: true
 }, {
 	name: 'GREEN',
+	image: 'assets/Green.png',
 	selected: true
 }, {
 	name: 'Hispanic Resource Group',
+	image: '',
 	selected: true
 }, {
 	name: 'NEW@Siemens',
+	image: 'assets/NewS.jpg',
 	selected: true
 }, {
 	name: 'Veteran’s Resource Group',
+	image: '',
+	selected: true
+}, {
+	name: 'PRIDE',
+	image: 'assets/pride.png',
 	selected: true
 }, {
 	name: 'Women’s Information Network',
+	image: 'assets/women.jpg',
 	selected: true
 }, {
 	name: 'Black Resource Group',
+	image: 'assets/brg.png',
 	selected: true
 }, {
 	name: 'BRIDGE',
+	image: 'assets/bridge.jpg',
 	selected: true
 }];
 
@@ -73,123 +86,175 @@ var speakers = [{
 var events = [{
 	title : "Engineering Week - DevJam Competition Launch",
 	site : sites[0],
-	date : "2015-02-23",
-	startTime: "09:00",
-	endTime: "10:00",
+	date : Date.parse("2015-02-23"),
+	startTime: Date.parse("09:00"),
+	endTime: Date.parse("10:00"),
 	image : 'assets/Siemens.png',
 	details: 'Very cool event',
 	location: 'Q1 2W31/32',
-	tags: tags,
-
+	groups: groups[0],
+	attendence: 'no',
+	rating: 1
+}, {
+	title : "Women and Financial Independence",
+	site : sites[0],
+	date : Date.parse("2015-04-10"),
+	startTime: Date.parse("13:00"),
+	endTime: Date.parse("15:00"),
+	image : 'assets/Siemens.png',
+	details: 'Very cool event',
+	location: 'Room 2E23',
+	groups: groups[7],
+	attendence: 'no',
+	rating: 1
+}, {
+	title : "Financial Planning for Millennial Generation",
+	site : sites[0],
+	date : Date.parse("2015-04-03"),
+	startTime: Date.parse("13:00"),
+	endTime: Date.parse("15:00"),
+	image : 'assets/Siemens.png',
+	details: 'Very cool event',
+	location: 'Room 2E23',
+	groups: groups[4],
+	attendence: 'no',
+	rating: 1
+}, {
+	title : "Engineering Week - The Language of Leadership",
+	site : sites[0],
+	date : Date.parse("2015-02-24"),
+	startTime: Date.parse("12:30"),
+	endTime: Date.parse("15:00"),
+	image : 'assets/Siemens.png',
+	details: 'Very cool event',
+	location: 'QIII 4N44-02',
+	groups: groups[7],
+	attendence: 'no',
+	rating: 1
 }, {
 	title : "Engineering Week",
 	site : sites[0],
-	date : "2015-02-04",
-	startTime: "13:00",
-	endTime: "15:00",
+	date : Date.parse("2015-02-04"),
+	startTime: Date.parse("13:00"),
+	endTime: Date.parse("15:00"),
 	image : 'assets/Siemens.png',
 	details: 'Very cool event',
 	location: 'Room 2E23',
-	tags: tags
-}, {
-	title : "Engineering Week Something Something Something Something",
-	site : sites[0],
-	date : "2015-02-04",
-	startTime: "13:00",
-	endTime: "15:00",
-	image : 'assets/Siemens.png',
-	details: 'Very cool event',
-	location: 'Room 2E23',
-	tags: tags
+	groups: groups[0],
+	attendence: 'no',
+	rating: 1
 }, {
 	title : "Engineering Week",
 	site : sites[0],
-	date : "2015-02-04",
-	startTime: "13:00",
-	endTime: "15:00",
+	date : Date.parse("2015-02-04"),
+	startTime: Date.parse("13:00"),
+	endTime: Date.parse("15:00"),
 	image : 'assets/Siemens.png',
 	details: 'Very cool event',
 	location: 'Room 2E23',
-	tags: tags
+	groups: groups[0],
+	attendence: 'no',
+	rating: 1
 }, {
 	title : "Engineering Week",
 	site : sites[0],
-	date : "2015-02-04",
-	startTime: "13:00",
-	endTime: "15:00",
+	date : Date.parse("2015-02-04"),
+	startTime: Date.parse("13:00"),
+	endTime: Date.parse("15:00"),
 	image : 'assets/Siemens.png',
 	details: 'Very cool event',
 	location: 'Room 2E23',
-	tags: tags
-}, {
-	title : "Engineering Week",
-	site : sites[0],
-	date : "2015-02-04",
-	startTime: "13:00",
-	endTime: "15:00",
-	image : 'assets/Siemens.png',
-	details: 'Very cool event',
-	location: 'Room 2E23',
-	tags: tags
-}, {
-	title : "Engineering Week",
-	site : sites[0],
-	date : "2015-02-04",
-	startTime: "13:00",
-	endTime: "15:00",
-	image : 'assets/Siemens.png',
-	details: 'Very cool event',
-	location: 'Room 2E23',
-	tags: tags
+	groups: groups[0],
+	attendence: "no",
+	rating: 1
 }];
 
 function createEvent() {
 	return {
 		title: "",
 		site: sites[0],
-		date: "2015-02-24",
-		startTime: "13:00",
-		endTime: "15:00",
+		date: Date.parse('next week'),
+		startTime: Date.parse('13:00'),
+		endTime: Date.parse('15:00'),
 		image: "",
 		hosts: {name: "", email: ""},
-		tags: jQuery.extend({}, tags)
+		speaker: {name: "", email: ""},
+		group: '',
+		rating: 1,
+		attendence: 'no',
+		feedback: ''
 	}
 }
 
+function sortNewer(a, b) {
+	if(a.date < b.date)
+		return -1;
+	if(a.date > b.date)
+		return 1;
+	if(a.startTime < b.startTime)
+		return -1;
+	if(a.startTime > b.startTime)
+		return 1;
+	return 0;
+}
+
+function sortOlder(a, b) {
+	if(a.date > b.date)
+		return -1;
+	if(a.date < b.date)
+		return 1;
+	if(a.startTime > b.startTime)
+		return -1;
+	if(a.startTime < b.startTime)
+		return 1;
+	return 0;
+}
+
+function arrayObjectIndexOf(myArray, searchTerm, property) {
+    for(var i = 0, len = myArray.length; i < len; i++) {
+    	console.log(property)
+    	console.log(searchTerm)
+        if (myArray[i][property] === searchTerm) return i;
+    }
+    return -1;
+}
+
 // Angular Handling 
-var eventsApp = angular.module('eventsApp', []);
+var eventsApp = angular.module('eventsApp', ['ui.bootstrap']);
 
 eventsApp.controller('appCtrl', function($scope){
+
+	$scope.currentEvent = null;
+	$scope.myEvents = [];
+	$scope.sites = sites;
+	$scope.groups = groups;
 
 	$scope.session = {
 		viewHome: true,
 		viewEvent: false,
 		editEvent: false,
-		rateEvent: false
-	}
+		rateEvent: false,
+		currentEvents: true,
+		pastEvents: false
+	};
 
-	$scope.currentEvent = null;
+	$scope.formatTime = function(dateObj) {
+		return dateObj.toString('h:ss');
+	};
 
-	$scope.myEvents = [];
-
-	$scope.sites = sites;
-
-	$scope.speakers = speakers;
-
-	$scope.tags = tags;
+	$scope.formatDate = function(dateObj) {
+		return dateObj.toString('dddd, MMMM d');
+	};
 	
 	$scope.viewHome = function() {
 		// Retreive data from server
-		$scope.filteredEvents = events;
-
 		// Load properties from cookie
-
-
-		// Show page
 		$scope.session.viewEvent = false;
 		$scope.session.editEvent = false;
 		$scope.session.rateEvent = false;
 		$scope.session.viewHome = true;
+
+		$scope.filterEvents();
 	};
 
 	$scope.viewEvent = function(event, editable) {
@@ -200,46 +265,74 @@ eventsApp.controller('appCtrl', function($scope){
 
 			$scope.currentEvent = createEvent();
 
-			$('.readonly').removeAttr('readonly');
+			$('.readonly').removeAttr('readonly', false);
 		} else {
 			$scope.session.viewHome = false;
 			$scope.session.viewEvent = true;
 			$scope.session.editEvent = false;
 
 			$scope.currentEvent = event;
-
-			$('.readonly').attr('readonly');
 			
 			if(Date.parse(event.date + " " + event.startTime) < Date.now()) {
 				$scope.session.rateEvent = true;
 			} else {
 				$scope.session.rateEvent = false;
 			}
+
+			$('.readonly').attr('readonly', true);
 		}
 	};
 
 	$scope.attendEvent = function() {
-		console.log($scope.currentEvent.startTime)
+		console.log($scope.currentEvent.startTime);
 	};
 
 	$scope.submitEvent = function() {
 		// Post $scope.session.currentEvent to server
-		
-		// Filter out unused tags
-		$scope.currentEvent.tags.filter(function(e) {
-			return e.selected;
-		});
+		console.log($scope.currentEvent.group)
 
-		// events.push($scope.currentEvent);
-
-		console.log($scope.currentEvent.startTime)
+		events.push($scope.currentEvent);
 	};
 
-	$scope.filterEvents = function() {
-		$scope.filteredEvents = events.filter(function(item) {
-			
+	$scope.filterEvents = function(click) {
+		if(click) {
+			$scope.session.currentEvents = $scope.session.currentEvents ? false : true;
+			$scope.session.pastEvents = $scope.session.pastEvents ? false : true;
+		}
+
+		if($scope.session.currentEvents) {
+			$scope.filteredEvents = events.filter(function(item) {
+				return item.date > Date.now();
+			});
+		} else {
+			$scope.filteredEvents = events.filter(function(item) {
+				return item.date <= Date.now();
+			});
+		}
+
+		// Filter Site
+		console.log($scope.sites)
+		$scope.filteredEvents = $scope.filteredEvents.filter(function(item) {
+			var index = arrayObjectIndexOf($scope.sites, item.site.name, 'name');
+			return $scope.sites[index] != undefined 
+				&&  $scope.sites[index].selected;
 		});
-	};
+
+		// Filter groups
+		$scope.filteredEvents = $scope.filteredEvents.filter(function(item) {
+			console.log(item)
+			var index = arrayObjectIndexOf($scope.groups, item.groups.name, 'name');
+			console.log(index)
+			return $scope.groups[index] != undefined 
+				&&  $scope.groups[index].selected;
+		});
+
+		if($scope.session.currentEvents) {
+			$scope.filteredEvents.sort(sortNewer);
+		} else {
+			$scope.filteredEvents.sort(sortOlder);
+		}
+	}
 
 	$scope.addHost = function() {
 		$scope.currentEvent.hosts.push({name: "", email: ""});
@@ -248,8 +341,4 @@ eventsApp.controller('appCtrl', function($scope){
 	$scope.addSpeaker = function() {
 		$scope.currentEvent.speakers.push({name: "", email: ""});
 	};
-
-	$scope.formatDate = function(date) {
-		return Date.parse(date).toString('dddd, MMMM d');
-	}
 });
